ad00f7fe656a510e3a04d8603b380f10f40b8191
xezii <amirzhan.zhampeisov01@gmail.com>
2026-05-16 14:34:02 +0500

    reorder(home): put 'Когда нужна диагностика?' (OurAdvantage) before BusinessModel

--- app/components/HomePage.tsx
+++ app/components/HomePage.tsx
@@ -24,9 +24,9 @@ export default function HomePage() {
       <main className="relative">
         <Navbar />
         <Hero />
-        <BusinessModel />
-        <Pricing />
         <OurAdvantage />
+        <Pricing />
+        <BusinessModel />
         <About />
         <Timeline />
         <Testimonials />
